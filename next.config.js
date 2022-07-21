16c331def38a7112c70b16aa0db4f2d9dcbfbd99
Andy <andrewfagan585@gmail.com>
2022-07-20 21:49:24 -0400

    env var updates

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    AUTH0_BASE_URL: process.env.AUTH0_BASE_URL || 'http://localhost:3000'
+    AUTH0_BASE_URL: process.env.AUTH0_BASE_URL || 'http://localhost:3000',
+    AUTH0_SECRET: process.env.AUTH0_SECRET,
+    AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET
   }
 }
 
