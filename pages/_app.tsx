c2f7134b05e989f903c5b44c04246b8edbaa7ad8
Andy <andrewfagan585@gmail.com>
2022-07-21 10:24:07 -0400

    build fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,9 @@ import { UserProvider } from '@auth0/nextjs-auth0'
 import type { AppProps } from 'next/app'
 import '/styles/index.scss'
 
+process.env.AUTH0_BASE_URL = process.env.AUTH0_BASE_URL || process.env.VERCEL_URL
+process.env.AUTH0_ISSUER_BASE_URL = process.env.AUTH0_ISSUER_BASE_URL
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,3 +1,6 @@
 import { handleAuth } from '@auth0/nextjs-auth0';
 
+process.env.AUTH0_BASE_URL = process.env.AUTH0_BASE_URL || process.env.VERCEL_URL
+process.env.AUTH0_ISSUER_BASE_URL = process.env.AUTH0_ISSUER_BASE_URL
+
 export default handleAuth();
