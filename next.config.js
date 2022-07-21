0a90769bf420fe491350233e335bce31f948782e
Andy <andrewfagan585@gmail.com>
2022-07-20 22:39:30 -0400

    test

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    AUTH0_BASE_URL: process.env.AUTH0_BASE_URL || 'http://localhost:3000',
+    AUTH0_BASE_URL: process.env.AUTH0_BASE_URL || 'https://wfh-buddy.netlify.app',
     AUTH0_SECRET: process.env.AUTH0_SECRET,
     AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL || 'https://wfh-buddy-prod.us.auth0.com',
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
