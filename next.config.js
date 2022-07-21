67f2550e6e4e1c5765f9be1fb987947bb1530303
Andy <andrewfagan585@gmail.com>
2022-07-21 10:17:29 -0400

    move to vercel and prod env

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    AUTH0_BASE_URL: process.env.AUTH0_BASE_URL || 'https://wfh-buddy.netlify.app',
+    AUTH0_BASE_URL: process.env.AUTH0_BASE_URL,
     AUTH0_SECRET: process.env.AUTH0_SECRET,
-    AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL || 'https://wfh-buddy-prod.us.auth0.com',
+    AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET
   }
