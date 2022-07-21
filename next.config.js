80c94cf6d18fc5a1d940ac4474601633b9fd7c55
Andy <andrewfagan585@gmail.com>
2022-07-20 21:43:14 -0400

    auth0 baseurl fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    AUTH0_BASE_URL: process.env.AUTH0_BASE_URL || 'http://localhost:3000'
+  }
 }
 
 module.exports = nextConfig
