5219f70ced1c8a315d4c6abcb914b4bda30b4e54
Andy <andrewfagan585@gmail.com>
2022-07-20 22:05:55 -0400

    test update

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     AUTH0_BASE_URL: process.env.AUTH0_BASE_URL || 'http://localhost:3000',
     AUTH0_SECRET: process.env.AUTH0_SECRET,
-    AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL || 'https://wfh-buddy-development.us.auth0.com',
+    AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL || 'https://wfh-buddy-prod.us.auth0.com',
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET
   }
